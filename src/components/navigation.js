f01d0e26b5304cd738014d17f1e76b42855a5801
Chuan Sun <chuansun.sc@gmail.com>
2020-04-24 16:28:51 -0500

    adding social networking logo

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,9 +2,14 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import IconButton from '@material-ui/core/IconButton';
 import Avatar from '@material-ui/core/Avatar';
 import { makeStyles } from '@material-ui/core/styles';
 import avatar_pic from "../static/images/avatar.jpg";
+import linkedin_logo from "../static/images/linkedin.png";
+import github_logo from "../static/images/github.png";
+import email_logo from "../static/images/email.png";
+
 import "../static/css/navigation.css";
 
 
@@ -22,6 +27,18 @@ function Navigation() {
         },
       }));
       const classes = useStyles();
+
+      const sc_ntwk_logo_click = (logo_name) => {
+          if (logo_name === "linkedin") {
+            window.open("https://www.linkedin.com/in/chuan-sun-2868646b/", '_blank');
+          }
+          else if (logo_name === "github") {
+            window.open("https://github.com/telenovelachuan", '_blank');
+          }
+          else if (logo_name === "email") {
+            window.open("mailto:chuansun.sc@gmail.com", '_blank');
+          }
+      };
     
   return (
     <div className="navigation" >
@@ -33,6 +50,24 @@ function Navigation() {
                 <div className="main_name">Chuan Sun</div>
                 <div className="main_subintro">
                     Data Science, <br /> Data Engineering
+                </div>
+                <br />
+                <div id="social_ntwk_area">
+                    <div className="sc_ntwk_logo">
+                        <IconButton onClick={()=>sc_ntwk_logo_click("linkedin")}>
+                            <Avatar src={linkedin_logo} style={{alignSelf: 'center'}} />
+                        </IconButton>
+                    </div>
+                    <div className="sc_ntwk_logo">
+                        <IconButton onClick={()=>sc_ntwk_logo_click("github")}>
+                            <Avatar src={github_logo} style={{alignSelf: 'center'}} />
+                        </IconButton>
+                    </div>
+                    <div className="sc_ntwk_logo">
+                        <IconButton onClick={()=>sc_ntwk_logo_click("email")}>
+                            <Avatar src={email_logo} style={{alignSelf: 'center'}} />
+                        </IconButton>
+                    </div>
                 </div>
                 
 
