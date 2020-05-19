3277d061539a1cb660b636b375b884da2f4c43cc
Chuan Sun <chuansun.sc@gmail.com>
2020-05-19 13:43:04 -0500

    static visualization scenarios

--- src/components/data_visualization.js
+++ src/components/data_visualization.js
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Chip from '@material-ui/core/Chip';
 import Avatar from '@material-ui/core/Avatar';
 import DoneIcon from '@material-ui/icons/Done';
+import CardMedia from '@material-ui/core/CardMedia';
 
 import "../static/css/dv.css";
 import json_file from './config/dv.json';
@@ -15,6 +16,12 @@ import d3js_logo from "../static/images/dv/d3js.png";
 import plotly_logo from "../static/images/dv/plotly.png";
 import jupyter_logo from "../static/images/dv/jupyter.png";
 import matlab_logo from "../static/images/dv/matlab.png";
+import ts1_logo from "../static/images/dv/ts1.png";
+import ts2_logo from "../static/images/dv/ts2.png";
+import ts3_logo from "../static/images/dv/ts3.png";
+import ca1_logo from "../static/images/dv/ca1.png";
+import wc1_logo from "../static/images/dv/wc1.png";
+import wc2_logo from "../static/images/dv/wc2.png";
 
 
 class DataVisualization extends Component {
@@ -24,7 +31,7 @@ class DataVisualization extends Component {
         this.state = {
 
         }
-        let tool_names = json_file["tools"];
+        // let tool_names = json_file["tools"];
         this.tools = {
             "Matplotlib": [matplotlib_logo],
             "Seaborn": [seaborn_logo],
@@ -36,7 +43,7 @@ class DataVisualization extends Component {
     }
 
     handleDelete = e => {
-        
+
     }
 
     load_intro_text = () => {
@@ -89,6 +96,120 @@ class DataVisualization extends Component {
 
                 </div>
             </div>
+
+
+            <div className="dv_static_aos" data-aos="fade-up" data-aos-offset="20" data-aos-delay="500"
+                    data-aos-duration="1000" data-aos-easing="ease-in-out-sine" data-aos-mirror="true" data-aos-id="aos_scroll_not_bottom"
+                    data-aos-once="true" >
+                <div className="dv_static">
+                <Card className={"dv_static_card"}>
+                    <div className="dv_static_card_aos" data-aos="fade-up" data-aos-offset="20" data-aos-delay="500"
+                        data-aos-duration="1000" data-aos-easing="ease-in-out-sine" data-aos-mirror="true" data-aos-id="aos_scroll_not_bottom"
+                        data-aos-once="true" >
+                        <CardHeader title="VISUALIZATION SCENARIOS" className="dv_static_header"
+                            titleTypographyProps={{className:"dv_headers"}}
+                        />
+                    </div>
+                    <CardContent className="dv_static_card_content">
+                        <div className="dv_static_projects">
+                            <div className="dv_static_left dv_static_area">
+                                <div className="dv_static_left_aos" data-aos="fade-up" data-aos-offset="20" data-aos-delay="500"
+                                    data-aos-duration="1000" data-aos-easing="ease-in-out-sine" data-aos-mirror="true"
+                                    data-aos-id="aos_scroll_not_bottom" data-aos-once="true">
+                                    <Card className={"dv_static_left_card"}>
+                                    <CardHeader title="Time Series Trend Prediction" className="dv_static_ts_header"
+                                        titleTypographyProps={{className:"dv_static_headers"}}
+                                    />
+                                    <CardContent className="dv_static_cc">
+                                        <div className="dv_static_ts_area dv_static_content">
+                                        <CardMedia className="dv_static_ts_cm" title={"time series prediction"} >
+                                            <img className="dv_static_img dv_static_ts_img" src={ts1_logo} alt="time series prediction"/>    
+                                        </CardMedia>
+                                        <CardMedia className="dv_static_ts_cm" title={"time series prediction"} >
+                                            <img className="dv_static_img dv_static_ts_img" src={ts3_logo} alt="time series prediction"/>    
+                                        </CardMedia>
+                                        </div>
+                                    </CardContent>
+                                    </Card>
+                                </div>
+                            </div>
+
+                            <div className="dv_static_middle dv_static_area">
+                                <div className="dv_static_middle_aos" data-aos="fade-up" data-aos-offset="20" data-aos-delay="500"
+                                    data-aos-duration="1000" data-aos-easing="ease-in-out-sine" data-aos-mirror="true"
+                                    data-aos-id="aos_scroll_not_bottom" data-aos-once="true">
+                                    <Card className={"dv_static_middle_card"}>
+                                    <CardHeader title="Topic Modeling" className="dv_static_wc_header"
+                                        titleTypographyProps={{className:"dv_static_headers"}}
+                                    />
+                                    <CardContent className="dv_static_cc">
+                                        <div className="dv_static_wc_area dv_static_content">
+                                            <CardMedia className="dv_static_wc_cm" title={"word cloud"} >
+                                                <img className="dv_static_img dv_static_wc_img" src={wc2_logo} alt="word cloud"/>    
+                                            </CardMedia>
+                                            <CardMedia className="dv_static_wc_cm" title={"word cloud"} >
+                                                <img className="dv_static_img dv_static_wc_img" src={wc1_logo} alt="word cloud"/>    
+                                            </CardMedia>
+                                            
+                                        </div>
+                                    </CardContent>
+                                    </Card>
+                                </div>
+                            </div>
+
+                            <div className="dv_static_right dv_static_area">
+                                <div className="dv_static_right_aos" data-aos="fade-up" data-aos-offset="20" data-aos-delay="500"
+                                    data-aos-duration="1000" data-aos-easing="ease-in-out-sine" data-aos-mirror="true"
+                                    data-aos-id="aos_scroll_not_bottom" data-aos-once="true">
+                                    <Card className={"dv_static_right_card"}>
+                                    <CardHeader title="Clustering Analysis" className="dv_static_ca_header"
+                                        titleTypographyProps={{className:"dv_static_headers"}}
+                                    />
+                                    <CardContent className="dv_static_cc">
+                                        <div className="dv_static_ca_area dv_static_content">
+                                            <CardMedia className="dv_static_ca_cm" title={"cluster analysis"} >
+                                                <img className="dv_static_img dv_static_ca_img" src={ca1_logo} alt="cluster analysis"/>    
+                                            </CardMedia>
+                                        </div>
+                                    </CardContent>
+                                    </Card>
+                                </div>
+                            </div>
+                        </div>
+                    </CardContent>
+                </Card>
+                </div>
+            </div>
+
+
+            <div className="dv_interactive_aos" data-aos="fade-up" data-aos-offset="20" data-aos-delay="500"
+                    data-aos-duration="1000" data-aos-easing="ease-in-out-sine" data-aos-mirror="true" data-aos-id="aos_scroll_not_bottom"
+                    data-aos-once="true" >
+                <div className="dv_interactive">
+                <Card className={"dv_interactive_card"}>
+                    <div className="dv_interactive_card_aos" data-aos="fade-up" data-aos-offset="20" data-aos-delay="500"
+                        data-aos-duration="1000" data-aos-easing="ease-in-out-sine" data-aos-mirror="true" data-aos-id="aos_scroll_not_bottom"
+                        data-aos-once="true" >
+                        <CardHeader title="INTERACTIVE STORYTELLING" className="dv_interactive_header"
+                            titleTypographyProps={{className:"dv_headers"}}
+                        />
+                    </div>
+                    <CardContent className="dv_interactive_card_content">
+                        <div className="dv_itr_example" id="dv_itr_3d">
+                                3d drag example
+                        </div>
+                        <div className="dv_itr_example" id="dv_itr_topic">
+                                topic dialog
+                        </div>
+                        <div className="dv_itr_example" id="dv_itr_geo">
+                                geo scattering
+                        </div>
+
+                    </CardContent>
+                </Card>
+                </div>
+            </div>
+
         </div>
     )}
 }
